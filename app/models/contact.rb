6cd3a7c9d996b3f2260b1f6c1aec08f25aa2aac2
Jeff Aschenbrenner <jeffrey.aschenbrenner@gmail.com>
2016-02-12 19:00:11 -0600

    Added welcome#index, contacts controller & mail_form gem

---
+++ app/models/contact.rb
@@ -0,0 +1,15 @@
+class Contact < MailForm::Base
+	attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  def headers 
+  	{
+	  	subject: 'My Industry Jobs - Contact Form',
+	  	# :to => "jtuggle@summitmarketing.solutions",
+	  	to: 'jeffrey.aschenbrenner@gmail.com',
+	  	from: %("#{name}" <#{email}>)
+	  }
+	end
+end
